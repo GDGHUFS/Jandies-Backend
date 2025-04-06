3e202a7a798923e37cbeb3da7f370e685760e1ee
daehyuh <rkdeown10@naver.com>
2025-04-06 13:51:15 +0900

    chore(init): Gradle 설정 및 .gitignore 구성

---
+++ src/test/java/gdghufs/jandies/JandiesBeApplicationTests.java
@@ -0,0 +1,13 @@
+package gdghufs.jandies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JandiesBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
